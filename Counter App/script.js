030b9ca0d1e24f111efee12b6e788f54f495d91a
ayashadevikar <ayashadevikar8@gmail.com>
2023-03-14 09:18:03 +0530

    counter app added

--- Counter App/script.js
+++ Counter App/script.js
@@ -24,7 +24,7 @@ function decrementValue(){
 }
 
 function resetValue(){
-  
-      document.getElementById("value").innerHTML = "";
    
+      document.getElementById("value").innerHTML = 0;
+      document.getElementById("value").style.color = "coral";
 }
